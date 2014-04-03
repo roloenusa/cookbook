c3f9bf64349c16ae1f5e498afad6f1673c5169d0
Juan Delgado <jdelgado@playmesh.com>
2014-04-03 09:58:31 -0700

    Updating binglogpurge recipe

--- cookbooks/mysql_binary_log_purge/recipes/default.rb
+++ cookbooks/mysql_binary_log_purge/recipes/default.rb
@@ -6,21 +6,23 @@
 # Cookbook Name:: mysql_binary_log_purge
 # Recipe:: default
 
-if node[:instance_role].include?('db_slave')
-  
-  template "/etc/engineyard/binlogpurge.yml" do
-    owner 'root'
-    group 'root'
-    mode 0644
-    source "binlogpurge.yml"
+if @node[:instance_role][/^db/]
+  remote_file "/engineyard/bin/binary_log_purge" do
+    source "binary_log_purge"
+    mode "744"
   end
-  
+
+  remote_file "/etc/engineyard/binlogpurge.yml" do
+    source "binlogpurge.yml"
+    mode "644"
+  end
+
   cron "binary_log_purge" do
     minute  '0'
     hour    '*/4'
     day     '*'
     month   '*'
     weekday '*'
-    command "/usr/local/ey_resin/bin/binary_log_purge -q"
+    command '/engineyard/bin/binary_log_purge -q'
   end
 end
